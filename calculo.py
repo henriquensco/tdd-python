1cf998a33275e2795e87db457429cc7546601c06
Henrique <nascimentocostahenrique@gmail.com>
2018-09-16 08:45:00 -0300

    Unittests com Python

---
+++ calculo.py
@@ -0,0 +1,18 @@
+#TEST DRIVE DEVELOPMENTE WITH PYTHON
+
+def soma(x, y):
+	#Realizar a soma dos produtos
+	return x + y
+
+def subtracao(x, y):
+	#Realizar a subtracao dos produtos
+	return x - y
+
+def divisao(x, y):
+	#Realizar a divisao dos produtos
+	return x / y
+
+def multiplicacao(x, y):
+	#Realizar a multiplicacao dos produtos
+	return x * y
+
---
+++ calculo_test.py
@@ -0,0 +1,27 @@
+import unittest
+import calculo
+
+class TestCalculo(unittest.TestCase):
+
+	def test_soma(self):
+		self.assertEqual(calculo.soma(10, 5), 15)
+		self.assertEqual(calculo.soma(-1, 1), 0)
+		self.assertEqual(calculo.soma(-1, -1), -2)
+
+	def test_subtracao(self):
+		self.assertEqual(calculo.subtracao(1, 1), 0)
+		self.assertEqual(calculo.subtracao(15, 8), 7)
+		self.assertEqual(calculo.subtracao(7, 3), 4)
+
+	def test_divisao(self):
+		self.assertEqual(calculo.divisao(5, 5), 1)
+		self.assertEqual(calculo.divisao(10, 5), 2)
+		self.assertEqual(calculo.divisao(10, 2), 5)
+
+	def test_multiplicacao(self):
+		self.assertEqual(calculo.multiplicacao(5, 5), 25)
+		self.assertEqual(calculo.multiplicacao(6, 3), 18)
+		self.assertEqual(calculo.multiplicacao(6, 6), 36)
+
+if __name__ == '__main__':
+	unittest.main()
